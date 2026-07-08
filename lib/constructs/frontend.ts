8e5e999e74af75a821c5a772251cde3c9af5de14
TatsuyaOoki <tat.ooki@outlook.jp>
2026-07-08 03:59:14 +0000

    feat: htmlファイルの新規追加

--- lib/constructs/frontend.ts
+++ lib/constructs/frontend.ts
@@ -1,5 +1,6 @@
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
@@ -11,10 +12,12 @@ export class Frontend extends Construct {
   constructor(scope: Construct, id: string, props: FrontendProps) {
     super(scope, id);
 
+    // CloudFront Distribution
     const distribution = new cloudfront.Distribution(this, "Web", {
       defaultBehavior: {
         origin: origins.S3BucketOrigin.withOriginAccessControl(props.webBucket),
       },
+      defaultRootObject: "index.html",
     });
   }
 }
