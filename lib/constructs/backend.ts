40fee790c95fa0d900c58e9557374afd58c553b6
TatsuyaOoki <tat.ooki@outlook.jp>
2026-07-08 11:28:42 +0000

    feat: API GatewayとLambda統合の追加

--- lib/constructs/backend.ts
+++ lib/constructs/backend.ts
@@ -1,6 +1,9 @@
 import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as nodejs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apigwv2 from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 import * as path from "path";
 
 export interface BackendProps {}
@@ -10,10 +13,27 @@ export class Backend extends Construct {
     super(scope, id);
 
     // Lambda
-    new nodejs.NodejsFunction(this, "Hello", {
+    const helloFunction = new nodejs.NodejsFunction(this, "Hello", {
       entry: path.join(__dirname, "../../lambda/hello/index.ts"),
       runtime: lambda.Runtime.NODEJS_24_X,
       handler: "handler",
+    });
+
+    // API Gateway
+    const integration = new HttpLambdaIntegration(
+      "HelloIntegration",
+      helloFunction,
+    );
+    const api = new apigwv2.HttpApi(this, "HttpApi", {});
+
+    api.addRoutes({
+      path: "/hello",
+      methods: [apigwv2.HttpMethod.GET],
+      integration: integration,
+    });
+
+    new cdk.CfnOutput(this, "HttpUrl", {
+      value: api.url!,
     });
   }
 }
